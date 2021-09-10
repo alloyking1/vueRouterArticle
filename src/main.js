676c2272a90bdbb9ab8baa00eb0fcddf390444f5
alloyking1 <alloyking1@gmail.com>
2021-09-10 11:46:28 -0400

    created a vue3 project, installed dependencies including bootstrap vue and vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import './plugins/bootstrap-vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+import Home from './components/HomeComponent'
+
+const routes = [
+  // { path: '/', component: () => import('./components/HomeComponent.vue') },
+  { path: '/', component: Home }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 new Vue({
   render: (h) => h(App),
+  router
 }).$mount('#app');
